973cff9ba3b998261fc62cbcfea205c853296f23
Shannon TJ <shannontj@shaw.ca>
2021-01-16 23:57:02 -0700

    redux imports

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
-import { StatusBar } from "expo-status-bar";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import rootReducer from "./redux/reducers";
+import thunk from "redux-thunk";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import LandingScreen from "./components/auth/Landing";
 import RegisterScreen from "./components/auth/Register";
 import LoginScreen from "./components/auth/Login";
+import MainScreen from "./components/Main";
 
 import * as firebase from "firebase";
 
@@ -15,6 +19,7 @@ if (firebase.apps.length === 0) {
   firebase.initializeApp(firebaseConfig);
 }
 
+const store = createStore(rootReducer, applyMiddleware(thunk));
 const Stack = createStackNavigator();
 
 export class App extends Component {
@@ -68,9 +73,9 @@ export class App extends Component {
       );
     }
     return (
-      <View style={{ flex: 1, justifyContent: "center" }}>
-        <Text>User is logged in...</Text>
-      </View>
+      <Provider store={store}>
+        <MainScreen />
+      </Provider>
     );
   }
 }
