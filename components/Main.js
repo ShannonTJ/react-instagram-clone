227a23ec688dc3ab53fbf9770ef8396e25c34de1
Shannon TJ <shannontj@shaw.ca>
2021-01-17 00:14:47 -0700

    fetches firebase data and displays username

--- components/Main.js
+++ components/Main.js
@@ -9,15 +9,24 @@ export class Main extends Component {
     this.props.fetchUser();
   }
   render() {
+    const { currentUser } = this.props;
+    console.log(currentUser);
+
+    if (currentUser == undefined) {
+      return <View></View>;
+    }
     return (
       <View style={{ flex: 1, justifyContent: "center" }}>
-        <Text>User is logged in...</Text>
+        <Text>{currentUser.name} is logged in...</Text>
       </View>
     );
   }
 }
 
-const mapDispatchProps = (dispatch) =>
+const mapStateToProps = (store) => ({
+  currentUser: store.userState.currentUser,
+});
+const mapDispatchToProps = (dispatch) =>
   bindActionCreators({ fetchUser }, dispatch);
 
-export default connect(null, mapDispatchProps)(Main);
+export default connect(mapStateToProps, mapDispatchToProps)(Main);
--- redux/actions/index.js
+++ redux/actions/index.js
@@ -10,7 +10,7 @@ export function fetchUser() {
       .get()
       .then((snapshot) => {
         if (snapshot.exists) {
-          dispatch({ type: USER_STATE_CHANGE, currentUser: snapshot.data });
+          dispatch({ type: USER_STATE_CHANGE, currentUser: snapshot.data() });
         } else {
           console.log("snapshot does not exist");
         }
