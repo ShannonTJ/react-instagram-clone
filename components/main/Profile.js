40cf7244a5cc4be0987290e90961d63baf2acc1e
Shannon TJ <shannontj@shaw.ca>
2021-01-21 15:59:54 -0700

    add following functions to redux

--- components/main/Profile.js
+++ components/main/Profile.js
@@ -121,6 +121,7 @@ const styles = StyleSheet.create({
 const mapStateToProps = (store) => ({
   currentUser: store.userState.currentUser,
   posts: store.userState.posts,
+  following: store.userState.following,
 });
 
 export default connect(mapStateToProps, null)(Profile);
