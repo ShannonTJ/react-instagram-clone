1a0fa9ea0193f515be6747c8e8eda399bee9ac03
Shannon TJ <shannontj@shaw.ca>
2021-01-22 15:06:21 -0700

    like and dislike buttons

--- components/main/Feed.js
+++ components/main/Feed.js
@@ -18,7 +18,32 @@ function Feed(props) {
       });
       setPosts(props.feed);
     }
+    console.log(posts);
   }, [props.usersFollowingLoaded, props.feed]);
+
+  const onLikePress = (userId, postId) => {
+    firebase
+      .firestore()
+      .collection("posts")
+      .doc(userId)
+      .collection("userPosts")
+      .doc(postId)
+      .collection("likes")
+      .doc(firebase.auth().currentUser.uid)
+      .set({});
+  };
+
+  const onDislikePress = (userId, postId) => {
+    firebase
+      .firestore()
+      .collection("posts")
+      .doc(userId)
+      .collection("userPosts")
+      .doc(postId)
+      .collection("likes")
+      .doc(firebase.auth().currentUser.uid)
+      .delete();
+  };
 
   return (
     <View style={styles.container}>
@@ -31,6 +56,17 @@ function Feed(props) {
             <View style={styles.containerImage}>
               <Text style={styles.container}>{item.user.name}</Text>
               <Image style={styles.image} source={{ uri: item.downloadURL }} />
+              {item.currentUserLike ? (
+                <Button
+                  title="Dislike"
+                  onPress={() => onDislikePress(item.user.uid, item.id)}
+                />
+              ) : (
+                <Button
+                  title="Like"
+                  onPress={() => onLikePress(item.user.uid, item.id)}
+                />
+              )}
               <Text
                 onPress={() =>
                   props.navigation.navigate("Comment", {
