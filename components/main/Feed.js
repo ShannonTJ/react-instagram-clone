ac8e21fc9590023c1e7b58a3702d864976b4cd85
Shannon TJ <shannontj@shaw.ca>
2021-01-21 18:32:46 -0700

    feed.js setup

--- components/main/Feed.js
+++ components/main/Feed.js
@@ -1,10 +1,56 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, View, Text, Image, FlatList, Button } from "react-native";
+import { connect } from "react-redux";
+import firebase from "firebase";
+require("firebase/firestore");
 
-export default function Feed() {
+function Feed(props) {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {}, [props.usersLoaded]);
+
+  if (user === null) {
+    return <View />;
+  }
+
   return (
-    <View>
-      <Text>Feed</Text>
+    <View style={styles.container}>
+      <View style={styles.containerGallery}>
+        <FlatList
+          numColumns={3}
+          horizontal={false}
+          data={userPosts}
+          renderItem={({ item }) => (
+            <View style={styles.containerImage}>
+              <Image style={styles.image} source={{ uri: item.downloadURL }} />
+            </View>
+          )}
+        />
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  containerInfo: { margin: 20 },
+  containerGallery: { flex: 1 },
+  image: {
+    flex: 1,
+    aspectRatio: 1 / 1,
+  },
+  containerImage: {
+    flex: 1 / 3,
+  },
+});
+
+const mapStateToProps = (store) => ({
+  currentUser: store.userState.currentUser,
+  following: store.userState.following,
+  users: store.usersState.users,
+  usersLoaded: store.usersState.usersLoaded,
+});
+
+export default connect(mapStateToProps, null)(Feed);
